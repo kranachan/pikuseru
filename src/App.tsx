0fe0c954a2f2f2763fc2716db00bd89aed202d70
Ayane <me@ayane.io>
2022-12-20 04:29:35 +0800

    feat: configure base theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import { Home } from 'pages/home'
 import './App.css'
+import { useEffect } from 'react'
+import { applyTheme } from 'themes'
+import { chestnut } from 'themes/chestnut'
+import { ColorScheme } from 'constants/theme'
 
 function App() {
+  useEffect(() => {
+    applyTheme(chestnut, ColorScheme.Light)
+  }, [])
+
   return (
     <main>
       <Routes>
---
+++ src/themes/chestnut.ts
@@ -0,0 +1,12 @@
+import { Theme } from 'constants/theme'
+
+export const chestnut: Theme = {
+  light: {
+    primary: '#3c221f',
+    background: '#e3cdc7',
+  },
+  dark: {
+    primary: '#f4a77b',
+    background: '#1d1412',
+  },
+}

6aef7e21779370582bcc0cb5e4703dd827a19591
Ayane <me@ayane.io>
2022-12-23 23:55:39 +0800

    feat: add `ButtonGroup` component

---
+++ src/components/button-group/index.tsx
@@ -0,0 +1,44 @@
+import clsx from 'clsx'
+import { Children, cloneElement, FC, isValidElement } from 'react'
+import styles from './index.module.css'
+
+type ButtonGroupProps = {
+  groupName?: string
+  styleForButtons?: string
+} & React.HTMLAttributes<HTMLDivElement>
+
+export const ButtonGroup: FC<ButtonGroupProps> = (props) => {
+  const { groupName, styleForButtons, className, children, ...rest } = props
+  const childrens = Children.toArray(children)
+  const groupedChildren = childrens.map((child, index) => {
+    if (!isValidElement(child)) {
+      return
+    }
+
+    const isFirst = index === 0
+    const isLast = index === childrens.length - 1
+    const borderClass = isFirst
+      ? styles['is-first']
+      : isLast
+      ? styles['is-last']
+      : styles['is-middle']
+
+    return cloneElement(child, {
+      ...child.props,
+      className: clsx(
+        child.props.className,
+        childrens.length !== 1 && borderClass,
+        styleForButtons,
+      ),
+    })
+  })
+
+  return (
+    <div className={styles['group-root']}>
+      <b className={styles['group-name']}>{groupName}</b>
+      <div className={clsx(styles['group-buttons'], className)} {...rest}>
+        {groupedChildren}
+      </div>
+    </div>
+  )
+}

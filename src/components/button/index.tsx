ac86373d4d9146ab1fb9e8cbf7269aff815e5576
Ayane <me@ayane.io>
2023-01-12 07:00:49 +0800

    feat(component): `Button` add `selected` prop

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,13 +2,20 @@ import clsx from 'clsx'
 import { FC } from 'react'
 import styles from './index.module.css'
 
-export type ButtonProps = {} & React.ButtonHTMLAttributes<HTMLButtonElement>
+export type ButtonProps = {
+  selected?: boolean
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
 
 export const Button: FC<ButtonProps> = (props) => {
-  const { className, children, disabled, ...rest } = props
+  const { className, children, disabled, selected, ...rest } = props
   return (
     <button
-      className={clsx(styles.button, disabled && styles.disabled, className)}
+      className={clsx(
+        styles.button,
+        disabled && styles.disabled,
+        selected && styles.selected,
+        className,
+      )}
       {...rest}
     >
       {children}

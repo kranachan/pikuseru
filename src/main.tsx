4717f8cbe36c8a1f091954f40c87f02734f532c2
Ayane <necromancer@minakami.land>
2023-02-17 16:59:00 +0800

    chore: configure react 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+if (!container) throw new Error('root element not found')
+const root = createRoot(container)
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
 )
